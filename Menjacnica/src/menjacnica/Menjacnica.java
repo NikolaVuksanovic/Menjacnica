2dd987b5b50bcdf3f101f36e3de4fc8035d38f9a
Nikola <nikolavuksanovic@gmail.com>
2017-04-04 22:57:38 -0700

    Implementirane metode u klasi Menjacnica

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,25 +1,44 @@
 package menjacnica;
 
+import java.util.ArrayList;
 import java.util.Date;
+
 
 public class Menjacnica implements IMenjacnica {
 
+	ArrayList<Kurs> listaValuta= new ArrayList<Kurs>();
 	@Override
-	public void dodajKursZaDan(Date datum, double kurs) {
+	public void dodajKursZaDan(Date datum, double kurs, Valuta val) {
 		// TODO Auto-generated method stub
-
+		Kurs noviKurs = new Kurs();
+		noviKurs.datum = datum;
+		noviKurs.kurs = kurs;
+		
+		listaValuta.add(noviKurs);
+		
 	}
 
 	@Override
 	public void izbrisiKursZaDan(Date datum) {
 		// TODO Auto-generated method stub
-
+		int i = 0;
+		while (i < listaValuta.size()) {
+			if (listaValuta.get(i).datum.equals(datum)) {
+				listaValuta.remove(i);
+			}
+		}
 	}
 
 	@Override
 	public double pronadjiKurs(Date datum) {
 		// TODO Auto-generated method stub
-		return 0;
+		int i = 0;
+		while (i < listaValuta.size()) {
+			if (listaValuta.get(i).datum.equals(datum)) {
+				return listaValuta.get(i).kurs;
+			}
+		}
+		return -1;
 	}
 
 }
