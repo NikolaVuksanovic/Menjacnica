d4db00242696f5312d9ad916ae430dc79cd28c6f
Nikola <nikolavuksanovic@gmail.com>
2017-04-04 23:00:09 -0700

    Implementrirane drugacije metode u klasi Menjacnica

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -7,18 +7,21 @@ public class Menjacnica implements IMenjacnica {
 	@Override
 	public void dodajKursZaDan(Date datum, double kurs) {
 		// TODO Auto-generated method stub
-
+		
+		System.out.println("Neki drugaciji kod!");
 	}
 
 	@Override
 	public void izbrisiKursZaDan(Date datum) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Neki drugaciji kod!");
 	}
 
 	@Override
 	public double pronadjiKurs(Date datum) {
 		// TODO Auto-generated method stub
+		System.out.println("Neki drugaciji kod!");
+
 		return 0;
 	}
 
