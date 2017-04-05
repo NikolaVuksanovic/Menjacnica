eb0fc364aaab9910b990965e4311c4bccb9467fb
Nikola <nikolavuksanovic@gmail.com>
2017-04-04 22:38:38 -0700

    Uvedena klasa Menjacnica

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,25 @@
+package menjacnica;
+
+import java.util.Date;
+
+public class Menjacnica implements IMenjacnica {
+
+	@Override
+	public void dodajKursZaDan(Date datum, double kurs) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void izbrisiKursZaDan(Date datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public double pronadjiKurs(Date datum) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
